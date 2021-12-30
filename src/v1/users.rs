4d84014597fd2fd657684407c9029d72d74bd407
Ronaldo <ronaldo.feitosa@mercadolivre.com>
2021-12-30 18:32:10 -0300

    Add mockal for mock tests

--- src/v1/users.rs
+++ src/v1/users.rs
@@ -26,24 +26,42 @@ fn path_config_handler(err: PathError, _req: &HttpRequest) -> actix_web::Error {
 
 #[cfg(test)]
 mod tests {
-    use actix_web::http::StatusCode;
-    use crate::MemoryRepository;
     use super::*;
+    use crate::user::User;
+    use crate::Error;
+    use mockall::*;
+    use mockall::predicate::*;
 
-    #[actix_rt::test]
-    async fn get_user_service_with_success() {
-        let res = get(web::Path::from(uuid::Uuid::parse_str("71802ecd-4eb3-4381-af7e-f737e3a35d5c").unwrap()),
-              RepositoryInjection::new(MemoryRepository::default())
-        ).await;
-        assert!(res.status().is_success());
-        assert_eq!(res.status(), StatusCode::OK);
+    mock! {
+        CustomRepo {}
+        impl Repository for CustomRepo {
+            fn get_user(&self, user_id: &uuid::Uuid) -> Result<User, Error>;
+        }
     }
 
     #[actix_rt::test]
-    async fn get_user_service_with_error() {
-        let res = get(web::Path::from(uuid::Uuid::parse_str("71802ecd-4eb3-4381-af7e-f737e3a35d5d").unwrap()),
-                  RepositoryInjection::new(MemoryRepository::default())
-        ).await;
-        assert_eq!(res.status(), StatusCode::NOT_FOUND);
+    async fn it_works() {
+        let user_id = uuid::Uuid::new_v4();
+        let user_name = "Meu nome";
+
+        let mut repo = MockCustomRepo::default();
+        repo.expect_get_user().returning(move |id| {
+            let mut user = User::new(user_name.to_string(), (1983, 08, 30));
+            user.id = *id;
+            Ok(user)
+        });
+
+        let mut result = get(web::Path::from(user_id), web::Data::new(repo)).await;
+        let user = result
+            .take_body()
+            .as_ref()
+            .map(|b| match b {
+                actix_web::dev::Body::Bytes(x) => serde_json::from_slice::<'_, User>(x).ok(), _ => None,
+            })
+            .flatten()
+            .unwrap();
+
+        assert_eq!(user.id, user_id);
+        assert_eq!(user.name, user_name);
     }
 }
